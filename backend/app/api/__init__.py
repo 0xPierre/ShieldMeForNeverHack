5d175a9f8745e9cd1f039b247b565918d5bd29f2
adrien <a.d44@tuta.io>
2025-12-04 22:13:46 +0100

    feat: Add phishing domains dataset and initialize phishing package

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import geo, health
+from app.api.routes import geo, health, phishing
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, tags=["health"])
 api_router.include_router(geo.router, prefix="/geo", tags=["geolocation"])
+api_router.include_router(phishing.router, prefix="/phishing", tags=["phishing"])
 
