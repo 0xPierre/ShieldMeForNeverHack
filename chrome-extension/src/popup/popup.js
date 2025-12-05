17402491b7d09f4409b09115d81056fefbab4d06
Loëvan Le Gac <loedu44@gmail.com>
2025-12-05 03:30:01 +0100

    feat: circle color depending on the score

--- chrome-extension/src/popup/popup.js
+++ chrome-extension/src/popup/popup.js
@@ -3,6 +3,13 @@ import { whois } from "../services/index.js";
 import { extractDomain, getBaseDomain } from "../services/domain.js";
 import { addTagToEmail, autoCompleteEmail } from "../services/mail.js";
 
+let nbLoaded, grade;
+
+document.addEventListener('DOMContentLoaded', () => {
+  nbLoaded = 0;
+  grade = 50;
+});
+
 /**
  * 
  * Manage the domain display in the popup
@@ -99,6 +106,19 @@ document.addEventListener('DOMContentLoaded', async () => {
     const [_, y, m, d] = result.creation_date.match(/(\d{4})-(\d{2})-(\d{2})/);
     const out = `${d}-${m}-${y}`;
     dateElement.innerText = out
+
+    // Calcul du score à enlever
+    const creationDate = new Date(`${y}-${m}-${d}T00:00:00Z`);
+    const now = new Date();
+
+    const diffTime = now - creationDate;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    const score = Math.max(0, 30 - diffDays);
+    grade -= score;
+    nbLoaded++;
+
+    animateRating(grade)
+
   } catch {
     console.error('Error looking up WHOIS for domain:', domain);
   }
@@ -106,9 +126,9 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
 /**
- * 
+ *
  * Manage mail tracking
- * 
+ *
  */
 
 
@@ -152,7 +172,7 @@ const getCurrentTaggedEmail = async (items) => {
 const applySettings = async (items) => {
   const mailSelector = document.getElementById('mail-selector');
   if (mailSelector) {
-    
+
     const currentTaggedEmail = await getCurrentTaggedEmail(items)
 
     mailSelector.value = currentTaggedEmail;
@@ -170,16 +190,66 @@ document.addEventListener('DOMContentLoaded', () => {
   const autofillButton = document.getElementById('autofill-button');
   const mailSelector = document.getElementById('mail-selector');
 
-  
+
   if (copyButton) {
     copyButton.addEventListener('click', () => {
       navigator.clipboard.writeText(mailSelector.value);
     });
   }
-  
+
   if (autofillButton) {
     autofillButton.addEventListener('click', () => {
       autoCompleteEmail(mailSelector.value);
     });
   }
 })
+
+function animateRating(value, duration = 800) {
+  const circle = document.getElementById("progressCircle");
+  const r = circle.r.baseVal.value;
+  const circumference = 2 * Math.PI * r;
+
+  const { grade, text } = getRatingData(value);
+
+  // Mettre à jour le texte
+  document.getElementById("ratingLetter").textContent = grade;
+  document.getElementById("ratingText").textContent = text;
+
+  document.getElementById("ratingSkeleton").classList.add("hidden");
+  document.getElementById("ratingContent").classList.remove("hidden");
+
+  // Couleur selon la valeur
+  if (value >= 80) circle.setAttribute("stroke", "rgb(11,218,94)");
+  else if (value >= 50) circle.setAttribute("stroke", "#f5c518");
+  else circle.setAttribute("stroke", "#ef4444");
+
+  circle.style.strokeDasharray = circumference;
+
+  const targetOffset = circumference - (value / 100) * circumference;
+
+  let start = null;
+
+  function animate(ts) {
+    if (start === null) start = ts;
+
+    const progress = Math.min((ts - start) / duration, 1);
+    const currentOffset = circumference * (1 - (value / 100) * progress);
+
+    circle.style.strokeDashoffset = currentOffset;
+
+    if (progress < 1) requestAnimationFrame(animate);
+  }
+
+  requestAnimationFrame(animate);
+}
+
+
+function getRatingData(value) {
+  if (value >= 90) return { grade: "A+", text: "Excellent" };
+  if (value >= 80) return { grade: "A", text: "Très bien" };
+  if (value >= 70) return { grade: "B", text: "Bien" };
+  if (value >= 60) return { grade: "C", text: "Correct" };
+  if (value >= 50) return { grade: "D", text: "Moyen" };
+  if (value >= 40) return { grade: "E", text: "Faible" };
+  return { grade: "F", text: "Insuffisant" };
+}
